fb6fcf05a819a336f6c582f94a148ab71b380ed2
jgjgill <dbdltm22@naver.com>
2024-04-03 19:11:16 +0900

    Node.js template engine study

--- packages/node-test/app.js
+++ packages/node-test/app.js
@@ -4,18 +4,21 @@ const express = require('express')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin.js')
-const shopRoutes = require('./routes/shop.js')
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
 })
 
 app.listen(3000)
