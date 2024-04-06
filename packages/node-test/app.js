a7c7d6fd48b71d58292b3327226688ade167c8fe
jgjgill <dbdltm22@naver.com>
2024-04-06 20:49:41 +0900

    chore: Node.js project setting

--- packages/node-test/app.js
+++ packages/node-test/app.js
@@ -1,24 +1,24 @@
-const path = require('path')
+const path = require('path');
 
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const errorController = require('./controllers/error');
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+const app = express();
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const errorController = require('./controllers/error')
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404)
+app.use(errorController.get404);
 
-app.listen(3000)
+app.listen(3000);
